32d6465ad23f5ddc509f1581785e3d6d33739428
Sam Ewdala <same7.hamada@gmail.com>
2016-12-03 23:34:16 +0200

    finishing the weather app

--- weather/index.ios.js
+++ weather/index.ios.js
@@ -21,20 +21,42 @@ var Weather = React.createClass({
   onRegionChangeComplete:function(region){
     let temp = [{'latitude':region.latitude,'longitude':region.longitude}];
     this.setState({pins:this.state.pins.concat([temp])});
-    Api(region.latitude,region.longitude).then((data) => {
-      // this.setState(data);
-      console.log(data)
+    Api.weather(region.latitude,region.longitude).then((data) => {
+      this.setState(data);
+      console.log(this.state.desc)
     });
     console.log(this.state.pins)
   },
   render:function(){
-    return <MapView onRegionChangeComplete={this.onRegionChangeComplete} annotations={this.state.pins.length > 0 ? this.state.pins[this.state.pins.length-1] : null} style={styles.map}/>
+    return <View style={styles.container}>
+            <MapView onRegionChangeComplete={this.onRegionChangeComplete} annotations={this.state.pins.length > 0 ? this.state.pins[this.state.pins.length-1] : null} style={styles.map}>
+            </MapView>
+          <View style={styles.textWrapper}>
+            <Text style={styles.text}>{this.state.city}</Text>
+            <Text style={styles.text}>{this.state.temp}</Text>
+            <Text style={styles.text}>{this.state.desc.charAt(0).toUpperCase() + this.state.desc.slice(1)}</Text>
+          </View>
+        </View>
   }
 });
 
 var styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'stretch',
+    backgroundColor:'#F5FCFF'
+  },
   map:{
-    flex:1
+    flex:2,
+    marginTop:20
+  },
+  textWrapper:{
+    flex:1,
+    alignItems:'center'
+  },
+  text:{
+    fontSize:30
   }
 });
 
--- weather/utils/api.js
+++ weather/utils/api.js
@@ -1,18 +1,37 @@
-var rootURL = 'api.openweathermap.org/data/2.5/weather?APPID=8e7e5ad91b6ab8cee2866027ff6491d1';
+var rootURL = 'http://api.openweathermap.org/data/2.5/weather?APPID=8e7e5ad91b6ab8cee2866027ff6491d1';
 var kelvinToC = function (kelvin){
-  return math.round(kelvin-273.15) + '˚C';
+  return Math.round(kelvin-273.15) + '˚C';
 }
-module.exports = function(lat,lon){
-  rootURL = `${rootURL}&lat=${lat}&lon=${lon}`;
-  return fetch(rootURL)
-    .then(function(response){
-      return response.json()
-    }).catch((error) => console.warn("fetch error:", error))
-    .then(function(json){
-      return {
-        city: json.city,
-        temp: kelvinToC(json.main.temp),
-        desc: json.weather[0].description
-      }
-    });
+module.exports = {
+  nully:function(lat,lon){
+    var URL = `${rootURL}&lat=${lat}&lon=${lon}`;
+    return fetch(URL)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(json){
+        console.log(response);
+        callback(responses);
+        return {
+          city: response.name,
+          temp: kelvinToC(response.main.temp),
+          desc: response.weather[0].description
+        }
+      }).catch((error) => console.warn("fetch error:", error));
+  },
+  weather:function (lat,lon) {
+    return fetch(`${rootURL}&lat=${lat}&lon=${lon}`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return {
+          city: responseJson.name,
+          temp: kelvinToC(responseJson.main.temp),
+          desc: responseJson.weather[0].description
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+    ;
+  }
 }
